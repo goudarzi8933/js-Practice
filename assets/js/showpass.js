5adb7f213e3fa7762ef0964f5b78d38a83cfa3a9
masoo <m.goudarzi8933@gmail.com>
2024-01-01 15:43:23 +0330

    update

--- assets/js/showpass.js
+++ assets/js/showpass.js
@@ -1,11 +1,40 @@
-        document.getElementById('showPass').onclick = function () {
-            document.getElementById('pass').type = 'text';
-            document.getElementById('showPass').style.display = 'none';
-            document.getElementById('hidePass').style.display = 'inline';
+        
+        
+        
+        //   document.getElementById('showPass').onclick = function () {
+        //     document.getElementById('password').type = 'text';
+        //     document.getElementById('showPass').style.display = 'none';
+        //     document.getElementById('hidePass').style.display = 'inline';
+        // }
+
+        // document.getElementById('hidePass').onclick = function () {
+        //     document.getElementById('password').type = 'password';
+        //     document.getElementById('showPass').style.display = 'inline';
+        //     document.getElementById('hidePass').style.display = 'none';
+        // }
+        
+        
+        
+        
+        
+        function changeToPass(){
+            document.getElementById('password').type = 'password';
+            document.getElementById('ShowPass').classList.add('fa-eye');
+            document.getElementById('ShowPass').classList.remove('fa-eye-slash');
+
+        }
+        function changeToText(){
+            document.getElementById('password').type = 'text';
+            document.getElementById('ShowPass').classList.remove('fa-eye');
+            document.getElementById('ShowPass').classList.add('fa-eye-slash');
         }
 
-        document.getElementById('hidePass').onclick = function () {
-            document.getElementById('pass').type = 'password';
-            document.getElementById('showPass').style.display = 'inline';
-            document.getElementById('hidePass').style.display = 'none';
+        document.getElementById('showPass').onmousedown = function () {
+            changeToText()
+        }
+        document.getElementById('showPass').onmouseup=function () {
+            changeToPass()
+        }
+        document.getElementById('showPass').onmouseleave=function () {
+            changeToPass()
         }
